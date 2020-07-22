e7d993ee17e769c6e4ca20122fdc03e1f8dca7c3
Georges Aaron RANDRIANAINA <randria-georges@outlook.com>
2020-07-22 18:52:56 +0200

    refactor(compilation/inc_exp): add a main for it exp in container

--- compilation/inc_exp.py
+++ compilation/inc_exp.py
@@ -1,105 +1,99 @@
+"""Incremental compilation experimentation
+"""
 import os
+import sys
 import subprocess
-from pathlib import Path
-LOGFILE = "stderr.output"
-data_dir = "data"
 
+
+def main():
+    if len(sys.argv) != 3:
+        print(\
+"""Usage:
+\tpython inc_expt <filename> <kernel>
+where
+\t<filename> is the file containing the configs to compile
+\t<kernel> is a path to the kernel source code
+""")
+        sys.exit()
+    file = sys.argv[1]
+    kernel_path = sys.argv[2]
+    os.system("mkdir inc scratch && cp -r {} inc/".format(kernel_path))
+    inc_launcher(kernel_path, file)
 
 
 def inc_launcher(kernel_path, file):
     rules = []
-    with open("file", 'r') as f:
+    with open(file, 'r') as f:
         rules = [l.strip('\n') for l in f.readlines()]
-    for i, r in enumerate(rules):
-        data_dir = data_dir[:-1] + str(i)
-        inc_building(kernel_path, r)
+    for rule in rules:
+        inc_build(kernel_path, [c.strip() for c in rule.split('->')])
 
 
-def compile(kernel_path, config, dest):
-    """Compiles a kernel in dest
-    
-    :param kernel_path: path to the kernel to compile
-    :type kernel_path: str
-    :param config: path to a configuration
-    :type config: str
-    :param dest: directory to compile the kernel in
-    :type dest: str
-    """
-    subprocess.run(args="make -C {} O={} {}".format(kernel_path, dest, config),
-                shell=True,
-                stdout=subprocess.DEVNULL,
-                stderr=LOGFILE,
-                check=True)
-    subprocess.run(args="make -C {} O={} -j4".format(kernel_path, dest),
-                shell=True,
-                stdout=subprocess.DEVNULL,
-                stderr=LOGFILE,
-                check=True)
-
-
-def inc_building(kernel_path, configs):
-    """Compiles incrementally some kernel according
-    to the given rules in configs
-    
-    :param kernel_path: path to the kernel
-    :type kernel_path: str
-    :param configs: list of rules 
-    :type configs: list(str)
-    """
-    dest = "kernel0"
-    compile(kernel_path, configs[0], dest)
-    check(dest)
-    config = configs[1:]
-    for i, config in enumerate(configs, 1):
+def inc_build(kernel_path, configs):
+    tmp_l = kernel_path.split('/')
+    if tmp_l[-1].strip() == "/":
+        tmp_l = tmp_l[:-1]
+    kernel_name = tmp_l[-1]
+    kernel_for_scratch = "scratch/{}".format(kernel_name)
+    kernel_for_inc = "inc/{}".format(kernel_name)
+    for config in configs:
+        compiled_from_scratch = "scratch/{}".format(config)
+        # scratch
+        os.system("cp {} {}".format(config, kernel_for_scratch))
+        os.system("make -C {} O=../{}"\
+                  .format(kernel_for_scratch, config))
+        os.system("make -C {} mrpropper".format(kernel_for_scratch))
         # incremental
-        compile(kernel_path, config, dest)
-        check(dest)
-        # from scratch
-        from_scratch = "{}-scratch-{}".format(config, i)
-        compile(kernel_path, config, from_scratch)
-        check(from_scratch)
-        bloat_o_meter_compare(kernel_path,
-                              dest, from_scratch,
-                              "{}/bloat-{}".format(data_dir, i))
+        os.system("cp {} {}".format(config, kernel_for_inc))
+        os.system("make -C {}".format(kernel_for_inc))
+        # check
+        os.system("cp {}/vmlinux {}/vmlinux.inc"\
+                  .format(kernel_for_inc, compiled_from_scratch))
+        # BLOAT-O-METER
+        # bloat-o-meter vmlinux vmlinux.inc
+        os.system(
+            "{}/scripts/bloat-o-meter {}/vmlinux {}/vmlinux.inc > {}/bloatsi"\
+                  .format(kernel_for_scratch, compiled_from_scratch,
+                          compiled_from_scratch, compiled_from_scratch))
+        # bloat-o-meter vmlinux.inc vmlinux
+        os.system(
+            "{}/scripts/bloat-o-meter {}/vmlinux.inc {}/vmlinux > {}/bloatis"\
+                  .format(kernel_for_scratch, compiled_from_scratch,
+                          compiled_from_scratch, compiled_from_scratch))
+        # VMSIZE
+        os.system('echo "scratch" >> {}/vmsizecompare'\
+                  .format(compiled_from_scratch))
+        os.system("echo $(du -sh vmlinux) >> {}/vmsizecompare"\
+                  .format(compiled_from_scratch))
+        os.system('echo "incremental" >> {}/vmsizecompare'\
+                  .format(compiled_from_scratch))
+        os.system("echo $(du -sh vmlinux.inc) >> {}/vmsizecompare"\
+                  .format(compiled_from_scratch))
+        # BUILT-IN
+        subprocess.run(
+            args=\
+            'find {} -name "built-in.o" | xargs size | sort -n -r -k 4\
+            > {}/builtinsize-scratch'\
+            .format(compiled_from_scratch, compiled_from_scratch),
+            shell=True, check=True)
+        if os.stat("builtinsize-scratch").st_size == 0:
+            subprocess.run(
+                args='find {} -name "built-in.a" | xargs size | sort -n -r -k 4\
+                > {}/builtinsize-scratch'\
+                .format(compiled_from_scratch, compiled_from_scratch),
+                shell=True, check=True)
+        subprocess.run(
+            args=\
+            'find {} -name "built-in.o" | xargs size | sort -n -r -k 4\
+            > {}/builtinsize-inc'.format(kernel_for_inc, compiled_from_scratch),
+            shell=True, check=True)
+        if os.stat("builtinsize-scratch").st_size == 0:
+            subprocess.run(
+                args='find {} -name "built-in.a" | xargs size | sort -n -r -k 4\
+                > {}/builtinsize-inc'\
+                .format(kernel_for_inc, compiled_from_scratch),
+                shell=True, check=True)
 
 
-"""
-CHECKERS
-"""
-
-def builtin_check(kernel, outputfn):
-    """Checks for the built-in files
-    """
-    subprocess.run(args='find {} -name "built-in.o" | xargs size |\
-    sort -n -r -k 4 > {}'.format(kernel, outputfn),
-                   shell=True)
-    if os.stat("output").st_size == 0:
-        print("*/built-in.o do not exist, checking for */built-in.a")
-        subprocess.run(args='find {} -name "built-in.a" | xargs size |\
-        sort -n -r -k 4 > {}'.format(kernel, outputfn),
-                    shell=True)
-
-
-def vmlinux_check(kernel, outputfn):
-    """Checks for the size of vmlinux
-    """
-    subprocess.run(args="du -sh {}/vmlinux > {}".format(kernel, outputfn),
-                   shell=True)
-
-
-def bloat_o_meter_compare(kernel_src, kernel_inc, kernel_scratch, outputfn):
-    """
-    """
-    subprocess.run(args='{}/scripts/bloat-o-meter {}/vmlinux\
-    {}/vmlinux > {}'.format(kernel_src,
-                            kernel_scratch,
-                            kernel_inc, outputfn),
-                   shell=True)
-
-
-checkers = [builtin_check, vmlinux_check]
-def check(dest):
-    """
-    """
-    for checker in checkers:
-        checker(dest, "{}/{}-{}".format(data_dir, checker.__name__, dest))
+if __name__ == "__main__":
+    main()
