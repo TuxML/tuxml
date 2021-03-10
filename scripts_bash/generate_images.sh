fb69bc69e223001c3f6e18cb343582d4fd94c172
FabienZa <fabien@zaplana.fr>
2021-03-10 16:02:52 +0100

    Fix security warning

--- scripts_bash/generate_images.sh
+++ scripts_bash/generate_images.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd docker_management/
-docker stop $(docker ps -a -q); 
-docker rm -f $(docker ps -a -q); 
-docker rmi -f $(docker images -q); 
+# Commented for securty reasons, to use only if necessary and never in production :
+# docker stop $(docker ps -a -q); 
+# docker rm -f $(docker ps -a -q); 
+# docker rmi -f $(docker images -q); 
 python3 docker_image_tuxml.py -f
 cd ../
