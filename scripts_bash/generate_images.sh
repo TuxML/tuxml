ebe59f6735d5da0c5ea7a02c34ff4b2d6c0820aa
FabienZa <fabien@zaplana.fr>
2021-04-21 22:19:47 +0200

    precise images who will deleted (tuxml)

--- scripts_bash/generate_images.sh
+++ scripts_bash/generate_images.sh
@@ -4,6 +4,7 @@ cd docker_management/
 # Commented for securty reasons, to use only if necessary and never in production :
 # docker stop $(docker ps -a -q); 
 # docker rm -f $(docker ps -a -q); 
-# docker rmi -f $(docker images -q); 
+# docker rmi $(docker images | grep 'tuxml/')
+# docker image prune -f
 python3 docker_image_tuxml.py -f
 cd ../
