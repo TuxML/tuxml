26f9610fdb8b2732bce9e46e4796263a96cf4fa5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 07:38:15 +0000

    Adding tarfile member sanitization to extractall()

--- miscellaneous/special-config/Kconfiglib/kernel_options_csv.py
+++ miscellaneous/special-config/Kconfiglib/kernel_options_csv.py
@@ -38,7 +38,26 @@ if __name__ == "__main__":
             wget_string = "http://cdn.kernel.org/pub/linux/kernel/v4.x/{}.tar.xz".format(kernel_string)
             linux_tar = wget.download(wget_string)
             with tarfile.open(linux_tar) as f:
-                f.extractall('.')
+                def is_within_directory(directory, target):
+                	
+                	abs_directory = os.path.abspath(directory)
+                	abs_target = os.path.abspath(target)
+                
+                	prefix = os.path.commonprefix([abs_directory, abs_target])
+                	
+                	return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                	for member in tar.getmembers():
+                		member_path = os.path.join(path, member.name)
+                		if not is_within_directory(path, member_path):
+                			raise Exception("Attempted Path Traversal in Tar File")
+                
+                	tar.extractall(path, members, numeric_owner=numeric_owner) 
+                	
+                
+                safe_extract(f, ".")
             os.remove(linux_tar)
 
     i = 0
